94fefd1d6022865f0c45307baf1fe886f99ce3fb
Yuta Nakamura <nakamurayuta0204@gmail.com>
2025-03-20 20:33:58 +0900

    Add null

--- vm/vm.go
+++ vm/vm.go
@@ -215,6 +215,8 @@ func (vm *VM) executeBanghOperator() error {
 		return vm.push(False)
 	case False:
 		return vm.push(True)
+	case Null:
+		return vm.push(True)
 	default:
 		return vm.push(False)
 	}
@@ -249,6 +251,9 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+
+	case *object.Null:
+		return false
 
 	default:
 		return true
